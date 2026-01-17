4fda0b45cd72f35265725b3af2f16472fd90aa1e
kluivstar <kasykasy610@gmail.com>
2026-01-17 06:16:47 +0100

    Override CaseStudy component on tablet

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -3,21 +3,21 @@ import Image from "../assets/casestudy-image.png"
 
 const CaseStudy = () => {
   return (
-    <section className='bg-white p-5'>
-        <div className='bg-slate-50 rounded-lg flex flex-col leading-6 py-8 px-5'>
+    <section className='bg-white p-5 md:p-10'>
+        <div className='bg-slate-50 rounded-lg flex flex-col leading-6 py-8 px-5 md:flex-row gap-6'>
             {/* Session Image */}
             <div>
-                <img src={Image} alt="" />
+                <img className='rounded-md' src={Image} alt="" />
             </div>
 
             {/* Session Details */}
             <div>
-                <h1 className='text-2xl font-semibold py-2'>Why Businesses Loves Cimplii</h1>
+                <h1 className='text-2xl font-semibold py-2 md:-mt-5'>Why Businesses Loves Cimplii</h1>
                 <p>With how insights are highly sort after by brands and organization Cimplii has become a valuable asset to help drive growth through valuable and concise insights. Decisions are make faster and efficiently without guesswork.</p>
                 
                 {/* Case Study Button */}
-                <div className='mx-auto text-center'>
-                    <button className="bg-black w-full text-white px-15 py-2 text-[15px] hover:bg-slate-300 hover:text-black">▪ Read the study</button>
+                <div className='mx-auto text-center md:text-left'>
+                    <button className="bg-black w-full text-white px-15 py-2 text-[15px] hover:bg-slate-300 hover:text-black md:w-auto">▪ Read the study</button>
                 </div>
             </div>
         </div>
