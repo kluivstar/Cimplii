d91b0dfa643088ba580b21f0f2b8ba5b9a927ca3
kluivstar <kasykasy610@gmail.com>
2026-01-19 15:47:57 +0100

    Override Testimonial section layout on lg-laptop screens

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,15 +4,15 @@ import TestimonialImage from '../assets/testImg.png';
 const Testimonial = () => {
   return (
     <section>
-        <div className='p-5 md:flex md:gap-10 md:py-8'>
+        <div className='p-5 md:flex md:gap-10 md:py-8 lg:px-5 lg:py-30'>
             {/* Testimonial Image */}
-            <div>
+            <div className='lg:w-1/2'>
                 <img src={TestimonialImage} alt="Testimonial" />
             </div>
-            <div>
-                <div className='py-2'>
+            <div className='lg:w-1/2'>
+                <div className='py-2 lg:mt-10 lg:px-12'>
                     <span className='text-4xl'>❝</span>
-                    <h1 className='text-2xl font-semibold -mt-4'>We finally exprienced what it means to truely work with valuable business data, it helped shape our company to what it has become.</h1>
+                    <h1 className='text-2xl font-semibold -mt-4 lg:text-5xl'>We finally exprienced what it means to truely work with valuable business data.</h1>
                     <p className='font-bold text-xl mt-5'>Patrick Star</p>
                     <span className='text-slate-700'>CEO, BB Inc.</span>
                 </div>
