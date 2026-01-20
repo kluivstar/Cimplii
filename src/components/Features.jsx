ccc7a19788e15230919949b482283b9ccf406b55
kluivstar <kasykasy610@gmail.com>
2026-01-20 08:57:35 +0100

    Override Features layout, added a count variable to list then rendered on 2xl-desktop screens

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,28 +4,32 @@ import FeatureImage from '../assets/image.png';
 const featureData = [
     {
         title: "Track",
+        count: "001",
         description: "Monitor your sustainability metrics in real-time",
     },
     {
         title: "Model",
+        count: "002",
         description: "Use advanced modeling tools to predict and plan initiatives.",
     },
     {
         title: "Act",
+        count: "003",
         description: "Implement actionable strategies based on data-driven insights.",
     },
     {
         title: "Report",
+        count: "004",
         description: "Generate comprehensive reports to share your progress",
     }
 ]
 
 const Features = () => {
   return (
-    <section className='bg-white p-5 md:mt-60 xl:mt-110'>
+    <section className='bg-white p-5 md:mt-60 xl:py-20 2xl:py-40'>
         {/* Features Headline */}
         <div className='py-10 lg:text-center'>
-            <h1 className="mx-auto text-2xl text-center md:text-3xl font-bold lg:w-1/2 xl:text-4xl">
+            <h1 className="mx-auto text-2xl text-center md:text-3xl font-bold lg:w-1/2 xl:text-4xl 2xl:text-5xl">
             Tools to measure, model, and act on sustainability, without guesswork.
             </h1>
 
@@ -42,17 +46,20 @@ const Features = () => {
                 {/* Features List */}
                 <div className='py-10'>
                     {featureData.map((feature, index) => (
-                        <div key = {index} className='py-3 lg:py-5'>
-                            <hr className='text-slate-200 lg:pt-2'/>
-                            <h2 className='text-[16px] md:text-xl font-semibold'>{feature.title}</h2>
-                            <p className='text-black md:text-lg lg:-mb-5'>{feature.description}</p>
+                        <div key = {index} className='py-3 lg:py-5 2xl:space-y-2'>
+                            <hr className='text-slate-200 lg:pt-2 2xl:pt-20'/>
+                            <div className='flex justify-between'>
+                                <h2 className='text-[16px] md:text-xl 2xl:text-3xl font-semibold'>{feature.title}</h2>
+                                <span className='text-black text-sm md:text-lg 2xl:text-2xl font-medium'>{feature.count}</span>
+                            </div>
+                            <p className='text-black md:text-lg lg:-mb-5 2xl:text-2xl'>{feature.description}</p>
                             
                         </div>
                     ))}
                 </div>
                 {/* Feature Button */}
                 <div className='mx-auto text-center lg:text-left'>
-                    <button className="bg-black text-white px-20 py-2 text-[15px] hover:bg-slate-300 hover:text-black md:w-full lg:w-1/2 lg:px-10 lg:py-2 xl:text-lg">▪ Explore features</button>
+                    <button className="bg-black text-white px-20 py-2 text-[15px] hover:bg-slate-300 hover:text-black md:w-full lg:w-1/2 lg:px-10 lg:py-2 xl:text-lg 2xl:text-2xl">▪ Explore features</button>
                 </div>
             </div>
         </div>
