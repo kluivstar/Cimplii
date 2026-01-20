1f6726bf90f37a214b1e830cac94f1f49d7f5f85
kluivstar <kasykasy610@gmail.com>
2026-01-20 08:48:09 +0100

    Override Hero layout  on 2xl-desktop screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,24 @@ import HeroImg from "../assets/hero-image.png";
 
 export default function Hero() {
   return (
-    <section className="hero px-5 lg:px-10">
-      <div className="text-center py-8 h-140 md:h-160 lg:h-140">
+    <section className="hero px-5 lg:px-10 2xl:px-0">
+      <div className="text-center py-8 h-140 md:h-160 lg:h-140 xl:h-170 2xl:h-190 2xl:py-20  ">
         {/* Hero Text */}
         <div className="flex flex-col md:gap-5">
-          <h2 className="text-[29px] md:text-6xl xl:text-7xl leading-5 tracking-wide lg:tracking-normal md:mt-10">Sustainability insights,</h2>
-          <h2 className="text-3xl font-semibold md:text-6xl xl:text-7xl">
+          <h2 className="text-[29px] md:text-6xl xl:text-7xl leading-5 tracking-wide lg:tracking-normal md:mt-10 2xl:text-8xl">Sustainability insights,</h2>
+          <h2 className="text-3xl font-semibold md:text-6xl xl:text-7xl 2xl:text-8xl">
         for modern businesses</h2>
         </div>
-        <p className="text-[16px] mt-2 font-semibold px-5 md:text-xl">
+        
+        <div>
+          <p className="text-[16px] mt-2 font-semibold px-5 md:text-xl 2xl:text-2xl">
           Measure impact, cut emissions, and make sustainability progress with confidence.
-        </p>
-        <Button />
+          </p>
+          <Button />
+        </div>
+
         {/* Hero Image */}
-        <div className="mt-5 md:mt-0 lg:px-20 lg:-mt-38 xl:ml-24">
+        <div className="flex mt-5 md:mt-0 lg:px-20 2xl:px-0 lg:-mt-38 xl:ml-24 2xl:ml-0 2xl:justify-center">
           <img src={HeroImg} className="outline-black" alt="" />
         </div>
       </div>
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div className='mt-4 flex flex-col gap-2 items-center md:flex-row md:justify-center lg:gap-5'>
-      <button className="bg-black text-white px-4 py-2 text-[15px] hover:bg-slate-300 hover:text-black xl:text-lg">▪ Request a demo</button>
-      <button className="bg-black text-white px-4 py-2 text-[15px] hover:bg-slate-300 hover:text-black xl:text-lg">▪ Explore the platform</button>
+    <div className='mt-4 flex flex-col gap-2 items-center md:flex-row md:justify-center lg:gap-5 2xl:pt-5'>
+      <button className="bg-black text-white px-4 py-2 text-[15px] hover:bg-slate-300 hover:text-black xl:text-lg 2xl:text-2xl">▪ Request a demo</button>
+      <button className="bg-black text-white px-4 py-2 text-[15px] hover:bg-slate-300 hover:text-black xl:text-lg 2xl:text-2xl">▪ Explore the platform</button>
 
     </div>
   )
