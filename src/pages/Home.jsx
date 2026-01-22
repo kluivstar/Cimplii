da4d1e09226bc4f0162f4c1115be1b5fabefa1af
kluivstar <kasykasy610@gmail.com>
2026-01-22 03:21:58 +0100

    feat: add multi-page routing architecture with shared layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Content from "../components/Content";
+import CaseStudy from "../components/CaseStudy";
+import Journal from "../components/Journal";
+import Testimonial from "../components/Testimonial";
+import CTA from "../components/CTA";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Content />
+      <CaseStudy />
+      <Journal />
+      <Testimonial />
+      <CTA />
+    </>
+  );
+}
