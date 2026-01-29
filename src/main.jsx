a24ba0ed4aaa5fb9842343df1eef5314e91030d4
Denzel <kasykasy610@gmail.com>
2026-01-29 16:31:30 +0100

    Added favicon

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
 
+// Bootstraps React - Enable routing - Mount App
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* Turn routing on - enable URL-based navigation - allow Routes, Route work */}
